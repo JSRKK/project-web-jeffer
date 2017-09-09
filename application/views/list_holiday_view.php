a61f270dbcf983050f09f3f650801d7795fe151f
jeyoung <jeyoungcs@gmail.com>
2017-09-10 00:03:52 +0700

    create controller&view holiday

---
+++ application/views/list_holiday_view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Holiday</title>
+</head>
+<body>
+    <div>
+        <h1><center>Project Jeffer</center></h1>
+    </div>
+</body>
+</html>
