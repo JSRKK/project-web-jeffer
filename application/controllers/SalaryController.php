6e12c801ef42c839c300fb497924e2ebff9f1c9a
jeyoung <jeyoungcs@gmail.com>
2017-09-13 20:38:09 +0700

    create salary view finish

--- application/controllers/SalaryController.php
+++ application/controllers/SalaryController.php
@@ -7,14 +7,22 @@ class SalaryController extends CI_Controller {
 	public function index()
 	{		
 		$this->load->model('SalaryModel');
+		$hours = $this->SalaryModel->get_hour();
 		$datas = $this->SalaryModel->get_data();
+		$temp = 0;
 		foreach ($datas as $row){
+			if($row['EMP_TYPE'] == "F"){
+				$temp = $row['EMP_SALARY'];
+			}
+			else{
+				$temp = $row['EMP_SALARY'] * $hours[0]['sum_hour'];
+			}		
 			$data[] = array(
 			'emp_id' => $row['EMP_ID'],
 			'full_name' => $row['EMP_TNAME']."  ".$row['EMP_FNAME']."  ".$row['EMP_LNAME'],					
 			'account' => $row['EMP_ACCOUNT'],			
 			'type' => $row['EMP_TYPE'],			
-			'rank' => $row['RANK_NAME']			
+			'rank' => $row['RANK_NAME']						
 		 	);	
 		}
 		$datas = $this->SalaryModel->get_salary();
@@ -22,17 +30,19 @@ class SalaryController extends CI_Controller {
 			$salary[] = array(
 			'date' => date("d/m/Y", strtotime($row['date'])),			
 			'ot1' => $row['ot1'],			
-			'ot2' => $row['ot2'],			
-			'ot2' => $row['ot2'],			
+			'ot2' => $row['ot2'],						
 			'diligence' => $row['diligence'],			
-			'bonus' => $row['bonus'],			
-			'income' => $row['income'],			
+			'bonus' => $row['bonus'],					
 			'insurance' => $row['insurance'],			
-			'mulct' => $row['mulct'],			
-			'quota' => $row['quota'],			
-			'total' => $row['total'],			
+			'holiday_pay' => $row['holiday_pay'],			
+			'late_pay' => $row['late_pay'],	
+			'other_pay' => $row['other_pay'],						
+			'total' => $temp,
+			'income_all' => $row['ot1']	+ $row['ot2'] +	$row['diligence'] + $row['bonus'] + $temp,
+			'pay_all' =>	$row['insurance'] + $row['holiday_pay'] + $row['late_pay'] + $row['other_pay']
 		 	);	
 		}
+		$dataShow['hour'] = $hours;
 		$dataShow['employee'] = $data;
 		$dataShow['salary'] = $salary;		
 		$this->load->view('salary_view', $dataShow);
--- application/models/SalaryModel.php
+++ application/models/SalaryModel.php
@@ -8,23 +8,31 @@ class SalaryModel extends CI_Model {
         }
 
         public function get_data(){
-                $this->db->select('EMP_TNAME, EMP_FNAME, EMP_LNAME, EMP_ACCOUNT, EMP_TYPE, rank.RANK_NAME, EMP_ID');
+                $this->db->select('EMP_TNAME, EMP_FNAME, EMP_LNAME, EMP_ACCOUNT, EMP_SALARY, EMP_TYPE, rank.RANK_NAME, EMP_ID');
                 $this->db->from('employee emp');
                 $this->db->join('rank','rank.RANK_ID = emp.RANK_ID');
-                $this->db->where('EMP_ID','JF0003');
+                $this->db->where('EMP_ID','JF0004');
                 $query = $this->db->get();               
                 return $query->result_array(); 
         } 
 
         public function get_salary(){
                 $this->db->select('SALARY_DATE as date, IFNULL(SALARY_OT1, 0) as ot1, IFNULL(SALARY_OT2, 0) as ot2, IFNULL(SALARY_DILIGENCE,0) as diligence, IFNULL(SALARY_BONUS,0) as bonus, 
-                                   IFNULL(SALARY_INCOME_OTHER,0) as income, IFNULL(SALARY_INSURANCE,0) as insurance, IFNULL(SALARY_MULCT_OTHER,0) as mulct, 
-                                   IFNULL(SALARY_EXCESS_QUOTA,0) as quota, SALARY_TOTAL as total');
+                                   IFNULL(SALARY_INSURANCE,0) as insurance, IFNULL(SALARY_HOLIDAY_PAY,0) as holiday_pay, IFNULL(SALARY_LATE_PAY,0) as late_pay, IFNULL(SALARY_OTHER_PAY,0) as other_pay, 
+                                   SALARY_TOTAL as total');
                 $this->db->from('salary');
-                $this->db->order_by("EMP_ID", "JF0003");
+                $this->db->order_by("EMP_ID", "JF0004");
                 $query = $this->db->get();               
                 return $query->result_array(); 
-        }     
+        }    
+        
+        public function get_hour(){
+                $this->db->select('SUM(SCHEDULE_HOURS) as sum_hour, SUM(SCHEDULE_OT1) as sum_ot1, SUM(SCHEDULE_OT2) as sum_ot2');
+                $this->db->from('schedule');
+                $this->db->where('EMP_ID','JF0004');
+                $query = $this->db->get();               
+                return $query->result_array(); 
+        } 
 }
 
 ?>
